0caec8732d4b9c2f208e4951d94931dd91f5e27e
Christian E. Hopps <chopps@gmail.com>
2016-12-23 09:59:39 -0500

    up version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
 
 
 setup (name='netconf',
-       version='0.4.2',
+       version='0.4.3',
        description='Netconf Client/Server Library',
        long_description=read("README.rst"),
        author='Christian E. Hopps',

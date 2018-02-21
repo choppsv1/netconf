d83298cedf6addaf2498996446deda541d81d0e5
Christian E. Hopps <chopps@gmail.com>
2018-02-20 21:24:18 -0500

    Add more features to CLI client.

--- netconf/cli.py
+++ netconf/cli.py
@@ -2,10 +2,20 @@
 #
 # August 23 2017, Christian Hopps <chopps@gmail.com>
 #
-# Copyright (c) 2017, Deutsche Telekom AG.
-# All Rights Reserved.
+# Copyright (c) 2017-2018, Deutsche Telekom AG
 #
-
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 from __future__ import absolute_import, division, unicode_literals, print_function, nested_scopes
 
 import argparse
@@ -13,19 +23,47 @@ import logging
 import os
 import sys
 import netconf.client as client
+from lxml import etree
+
+
+def parse_password_arg(password):
+    if password:
+        if password.startswith("env:"):
+            unused, key = password.split(":", 1)
+            password = os.environ[key]
+        elif password.startswith("file:"):
+            unused, path = password.split(":", 1)
+            password = open(path).read().rstrip("\n")
+    return password
 
 
 def main(*margs):
     parser = argparse.ArgumentParser("Netconf Client Utility")
     parser.add_argument("--debug", action="store_true", help="Enable debug logging")
     parser.add_argument('--host', default="localhost", help='Netconf server hostname')
-    parser.add_argument('--port', default="830", help='Netconf server port')
+    parser.add_argument(
+        '--get',
+        nargs='?',
+        help="Perform <get>. arg value is xpath/xml filter or taken from infile if not specified")
+    parser.add_argument(
+        '--get-config',
+        nargs='?',
+        help=
+        "Perform <get-config>. arg value is xpath/xml filter or taken from infile if not specified")
     parser.add_argument(
         '--hello', action="store_true", help="Do hello and return capabilities of server.")
     parser.add_argument("-i", "--infile", help="File to read from")
-    parser.add_argument('-p', '--password', default=None, help='Netconf password')
-    parser.add_argument('--passenv', default=None, help='Environment variable holding password')
+    parser.add_argument(
+        '-p',
+        '--password',
+        default=None,
+        help='Netconf password (use "env:" or "file:" prefix to specify source)')
+    # Deprecated now parse password args more functional
+    parser.add_argument('--passenv', default=None, help=argparse.SUPPRESS)
+    parser.add_argument('--port', type=int, default=830, help='Netconf server port')
     parser.add_argument("-q", "--quiet", action="store_true", help="Quiet operation")
+    parser.add_argument('--source', default="running", help="Source for get config")
+    parser.add_argument('--timeout', type=float, help="Timeout for command in fractional seconds")
     parser.add_argument('-u', '--username', default="admin", help='Netconf username')
     parser.add_argument("-v", "--verbose", action="store_true", help="Verbose logging")
     args = parser.parse_args(*margs)
@@ -35,6 +73,8 @@ def main(*margs):
         sys.exit(1)
     if args.passenv:
         args.password = os.environ[args.passenv]
+    else:
+        args.password = parse_password_arg(args.password)
 
     if args.debug:
         logging.basicConfig(level=logging.DEBUG)
@@ -47,16 +87,25 @@ def main(*margs):
 
     session = client.NetconfSSHSession(
         args.host, args.port, args.username, args.password, debug=args.debug)
-    if args.hello:
-        print("\n".join(session.capabilities))
-        sys.exit(0)
 
-    if args.infile:
-        xml = open(args.infile).read()
+    if args.hello:
+        result = "\n".join(session.capabilities) + "\n"
+    elif args.get:
+        result = session.get(args.get, args.timeout)
+        result = etree.tounicode(result, pretty_print=True)
+    elif args.get_config:
+        result = session.get_config(args.source, args.get_config, args.timeout)
+        result = etree.tounicode(result, pretty_print=True)
     else:
-        xml = sys.stdin.read()
-    result = session.send_rpc(xml)
-    sys.stdout.write(result[2])
+        if args.infile:
+            xml = open(args.infile).read()
+        else:
+            xml = sys.stdin.read()
+        if not xml:
+            print("Nothing to do.", file=sys.stderr)
+            sys.exit(1)
+        result = session.send_rpc(xml)[2]
+    sys.stdout.write(result)
     session.close()
 
 

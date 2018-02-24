3cc8cad50c1b3f60214fdb59c93d7e48ec4537cc
Christian E. Hopps <chopps@gmail.com>
2018-02-24 14:21:55 -0500

    Autodoc not working on readthedocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,10 +20,14 @@ import sys
 sys.path.insert(0, os.path.abspath('../..'))
 # -- General configuration ----------------------------------------------------
 
+autodoc_default_flags = ['members']
+autosummary_generate = True
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     #'sphinx.ext.intersphinx',
     #'sphinx.ext.todo",
 ]
